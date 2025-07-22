a9fdefbb80989832219e15d0ebf06b49188e7430
satyamadhavarapu <satyamadhavarapu28@gmail.com>
2025-07-22 16:56:00 +0530

    Initial commit

---
+++ Week 5/Microservices/loan/src/main/java/com/cognizant/loan/LoanController.java
@@ -0,0 +1,15 @@
+package com.cognizant.loan;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Loan getLoan(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
+
+    record Loan(String number, String type, double loan, double emi, int tenure) {}
+}
